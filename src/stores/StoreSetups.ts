0eb9cca47bc2e7b2f45952b44015ca583890b1ef
Heinrich Muralt <heinrich@supertext.ch>
2018-07-17 17:30:56 +0200

    Removed unused router code

--- src/stores/StoreSetups.ts
+++ src/stores/StoreSetups.ts
@@ -1,18 +1,14 @@
-import { routerMiddleware, routerReducer  } from "react-router-redux";
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { createStore } from "redux";
 import { CoreRegistry } from "typestately";
-import history from "../history";
 
 export function setupMainStore() {
-    const initalReducers = {
-        router: routerReducer
-    };
+    const store = createStore((state) => state);
 
-    const store = createStore(combineReducers(initalReducers), applyMiddleware(routerMiddleware(history)));
     store.subscribe(() => {
         // Just for testing/demo purpose see how the store is changing
         // tslint:disable-next-line:no-console
         console.log(store.getState());
     })
-    return CoreRegistry.registerStore(store, initalReducers);
+
+    return CoreRegistry.registerStore(store, {});
 }
